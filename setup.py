c4fb48b5953a34f6625287201289541f36794e88
yasminenahal <yasmine.nahal@aalto.fi>
2024-09-27 15:22:24 +0200

    first commit

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name="reinvent_hitl_scoring",
-    version="0.0.5",
+    name="reinvent_scoring",
+    version="0.0.75",
     author="Yasmine Nahal",
     author_email="yasminenahal@aalto.fi",
     description="Scoring functions for Reinvent adapted to the HITL_AL_GOMG workflow",
-    url="https://github.com/yasminenahal/reinvent-hitl-scoring.git",
+    url="https://github.com/yasminenahal/reinvent-scoring.git",
     package_data={"reinvent_scoring": ["scoring/score_components/synthetic_accessibility/fpscores.pkl.gz"]},
     packages=setuptools.find_packages(exclude='unittest_reinvent'),
     classifiers=[
